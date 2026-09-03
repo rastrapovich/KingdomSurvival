c671f7252d6b2388b00ae8fedd509e07343e3b32
rastrapovich <125567783+rastrapovich@users.noreply.github.com>
2026-09-03 14:23:26 +0300

    Add combat tag modifier regression tests

---
+++ Assets/_Project/BattleSandbox/Tests/EditMode/CombatTagModifierTests.cs
@@ -0,0 +1,266 @@
+using NUnit.Framework;
+
+namespace KingdomSurvival.BattleSandbox.Tests
+{
+    public sealed class CombatTagModifierTests
+    {
+        [Test]
+        public void Guard_UsesFiftyPercentDefenseBonus()
+        {
+            SandboxUnitState defender = CreateState(
+                "defender",
+                SandboxTeam.Player,
+                2,
+                2,
+                2,
+                1,
+                new[] { SandboxCombatTagRules.Human });
+            SandboxUnitState enemy = CreateState(
+                "enemy",
+                SandboxTeam.Enemy,
+                2,
+                2,
+                1,
+                1,
+                new[] { SandboxCombatTagRules.Beast });
+            SandboxBattle battle = CreateBattle(defender, enemy);
+            battle.Start();
+
+            Assert.That(battle.TryGuard(defender.Id, out _), Is.True);
+            Assert.That(SandboxCombatTagRules.GetEffectiveDefense(defender), Is.EqualTo(3m));
+        }
+
+        [Test]
+        public void DefenderAndArmored_UseFlatArmorBeforeSeventyFivePercentGuardBonus()
+        {
+            SandboxUnitState defender = CreateState(
+                "defender",
+                SandboxTeam.Player,
+                2,
+                4,
+                2,
+                1,
+                new[]
+                {
+                    SandboxCombatTagRules.Human,
+                    SandboxCombatTagRules.Defender,
+                    SandboxCombatTagRules.Armored
+                });
+            SandboxUnitState enemy = CreateState(
+                "enemy",
+                SandboxTeam.Enemy,
+                2,
+                2,
+                1,
+                1,
+                new[] { SandboxCombatTagRules.Beast });
+            SandboxBattle battle = CreateBattle(defender, enemy);
+            battle.Start();
+
+            Assert.That(SandboxCombatTagRules.GetEffectiveDefense(defender), Is.EqualTo(6m));
+            Assert.That(battle.TryGuard(defender.Id, out _), Is.True);
+            Assert.That(SandboxCombatTagRules.GetEffectiveDefense(defender), Is.EqualTo(10.5m));
+        }
+
+        [Test]
+        public void BeastSlayer_AddsFiftyPercentAttackAgainstBeast()
+        {
+            SandboxUnitState attacker = CreateState(
+                "spearman",
+                SandboxTeam.Player,
+                3,
+                3,
+                3,
+                1,
+                new[] { SandboxCombatTagRules.Human, SandboxCombatTagRules.BeastSlayer });
+            SandboxUnitState target = CreateState(
+                "beast",
+                SandboxTeam.Enemy,
+                2,
+                1,
+                3,
+                1,
+                new[] { SandboxCombatTagRules.Beast });
+
+            Assert.That(
+                SandboxCombatTagRules.GetEffectiveAttack(attacker, target, attacker.Position),
+                Is.EqualTo(4.5m));
+        }
+
+        [Test]
+        public void HumanSlayer_AddsFiftyPercentAttackAgainstHuman()
+        {
+            SandboxUnitState attacker = CreateState(
+                "alpha",
+                SandboxTeam.Enemy,
+                4,
+                3,
+                5,
+                1,
+                new[] { SandboxCombatTagRules.Beast, SandboxCombatTagRules.HumanSlayer });
+            SandboxUnitState target = CreateState(
+                "human",
+                SandboxTeam.Player,
+                2,
+                2,
+                3,
+                1,
+                new[] { SandboxCombatTagRules.Human });
+
+            Assert.That(
+                SandboxCombatTagRules.GetEffectiveAttack(attacker, target, attacker.Position),
+                Is.EqualTo(6m));
+        }
+
+        [Test]
+        public void RangedTag_HalvesAttackAtAdjacentHex()
+        {
+            SandboxUnitState attacker = CreateState(
+                "archer",
+                SandboxTeam.Player,
+                3,
+                1,
+                3,
+                4,
+                new[] { SandboxCombatTagRules.Human, SandboxCombatTagRules.Ranged });
+            SandboxUnitState target = CreateState(
+                "target",
+                SandboxTeam.Enemy,
+                2,
+                2,
+                3,
+                1,
+                new[] { SandboxCombatTagRules.Beast },
+                new HexCoord(1, 0));
+
+            Assert.That(
+                SandboxCombatTagRules.GetEffectiveAttack(attacker, target, attacker.Position),
+                Is.EqualTo(1.5m));
+        }
+
+        [Test]
+        public void RangedTag_KeepsFullAttackBeyondAdjacentHex()
+        {
+            SandboxUnitState attacker = CreateState(
+                "archer",
+                SandboxTeam.Player,
+                3,
+                1,
+                3,
+                4,
+                new[] { SandboxCombatTagRules.Human, SandboxCombatTagRules.Ranged });
+            SandboxUnitState target = CreateState(
+                "target",
+                SandboxTeam.Enemy,
+                2,
+                2,
+                3,
+                1,
+                new[] { SandboxCombatTagRules.Beast },
+                new HexCoord(3, 0));
+
+            Assert.That(
+                SandboxCombatTagRules.GetEffectiveAttack(attacker, target, attacker.Position),
+                Is.EqualTo(3m));
+        }
+
+        [Test]
+        public void PreviewAttack_UsesTagDrivenRangedPenalty()
+        {
+            SandboxUnitState archer = CreateState(
+                "archer",
+                SandboxTeam.Player,
+                3,
+                2,
+                4,
+                4,
+                new[] { SandboxCombatTagRules.Human, SandboxCombatTagRules.Ranged });
+            SandboxUnitState target = CreateState(
+                "target",
+                SandboxTeam.Enemy,
+                1,
+                2,
+                3,
+                1,
+                new[] { SandboxCombatTagRules.Beast },
+                new HexCoord(1, 0));
+            SandboxBattle battle = CreateBattle(archer, target);
+            battle.Start();
+
+            SandboxAttackPreview preview = battle.PreviewAttack(archer.Id, target.Id);
+
+            Assert.That(preview.IsValid, Is.True);
+            Assert.That(preview.Damage, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Retaliation_UsesHumanSlayerAttackModifier()
+        {
+            SandboxUnitState human = CreateState(
+                "human",
+                SandboxTeam.Player,
+                2,
+                2,
+                2,
+                1,
+                new[] { SandboxCombatTagRules.Human });
+            SandboxUnitState alpha = CreateState(
+                "alpha",
+                SandboxTeam.Enemy,
+                4,
+                3,
+                5,
+                1,
+                new[]
+                {
+                    SandboxCombatTagRules.Beast,
+                    SandboxCombatTagRules.HumanSlayer
+                },
+                new HexCoord(1, 0));
+            SandboxBattle battle = CreateBattle(human, alpha);
+            battle.Start();
+
+            Assert.That(battle.TryAttack(human.Id, alpha.Id, out _), Is.True);
+            SandboxAttackPreview retaliation = battle.PreviewPendingRetaliation();
+
+            Assert.That(retaliation.IsValid, Is.True);
+            Assert.That(retaliation.Damage, Is.EqualTo(10));
+        }
+
+        private static SandboxBattle CreateBattle(
+            SandboxUnitState player,
+            SandboxUnitState enemy)
+        {
+            return new SandboxBattle(8, 4, new[] { player, enemy });
+        }
+
+        private static SandboxUnitState CreateState(
+            string id,
+            SandboxTeam team,
+            int attack,
+            int defense,
+            int damage,
+            int attackRange,
+            string[] tags,
+            HexCoord? position = null)
+        {
+            SandboxUnitDefinition definition = new SandboxUnitDefinition(
+                id + "_type",
+                id,
+                team == SandboxTeam.Enemy ? SandboxUnitRole.Beast : SandboxUnitRole.Militia,
+                30,
+                attack,
+                defense,
+                damage,
+                3,
+                team == SandboxTeam.Player ? 10 : 1,
+                attackRange,
+                tags);
+            return new SandboxUnitState(
+                id,
+                definition,
+                team,
+                position ?? new HexCoord(0, 0));
+        }
+    }
+}
